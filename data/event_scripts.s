d85ad6752375a56f926b9ea54504efd10572a243
OceanMan-sudo <nd799@hotmail.com>
2024-09-06 23:23:27 -0700

    Pallet Town Expansion and Route 1 Update
    This list of changes include initial tutorial of how to use PoryMap and applying it to Pallet Town. Added two homes with one being accessible, and Pikachu and Eevee to Route 1

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1360,3 +1360,6 @@ Text_TestMsg::
 	.include "data/text/save.inc"
 	.include "data/text/new_game_intro.inc"
 	.include "data/text/pokedude.inc"
+
+	.include "data/maps/PalletTown_NewHome1_1F/scripts.inc"
+	.include "data/maps/PalletTown_NewHome1_1F/text.inc"
