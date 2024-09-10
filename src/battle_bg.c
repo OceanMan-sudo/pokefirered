d2bb7ac662a7e7fd7ff48c0c3c0f41aed80d27a2
OceanMan-sudo <nd799@hotmail.com>
2024-09-09 20:47:11 -0700

    PSS Icons
    Added PSS Icons. Removed PP Remaining message in battle window. Everything works great!

--- src/battle_bg.c
+++ src/battle_bg.c
@@ -203,19 +203,10 @@ static const struct WindowTemplate sStandardBattleWindowTemplates[] = {
         .paletteNum = 5,
         .baseBlock = 0x330
     },
-    [B_WIN_PP] = {
-        .bg = 0,
-        .tilemapLeft = 21,
-        .tilemapTop = 55,
-        .width = 3,
-        .height = 2,
-        .paletteNum = 5,
-        .baseBlock = 0x290
-    },
     [B_WIN_MOVE_TYPE] = {
         .bg = 0,
         .tilemapLeft = 21,
-        .tilemapTop = 57,
+        .tilemapTop = 55,
         .width = 8,
         .height = 2,
         .paletteNum = 5,
@@ -223,21 +214,21 @@ static const struct WindowTemplate sStandardBattleWindowTemplates[] = {
     },
     [B_WIN_PP_REMAINING] = {
         .bg = 0,
-        .tilemapLeft = 24,
-        .tilemapTop = 55,
+        .tilemapLeft = 23,
+        .tilemapTop = 57,
         .width = 5,
         .height = 2,
         .paletteNum = 5,
         .baseBlock = 0x2a6
     },
-    [B_WIN_DUMMY] = {
+    [B_WIN_PSS_ICON] = {
         .bg = 0,
-        .tilemapLeft = 25,
+        .tilemapLeft = 21,
         .tilemapTop = 57,
-        .width = 0,
-        .height = 0,
-        .paletteNum = 5,
-        .baseBlock = 0x2b0
+        .width = 2,
+        .height = 2,
+        .paletteNum = 10,
+        .baseBlock = 0x2b1
     },
     [B_WIN_SWITCH_PROMPT] = {
         .bg = 0,
