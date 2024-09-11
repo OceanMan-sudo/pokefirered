67c053a523a22c3ff4da9228e10790356bbb4da2
OceanMan-sudo <nd799@hotmail.com>
2024-09-10 19:46:44 -0700

    NPC in Pallet Town New Home
    added npc

--- data/maps/PalletTown_NewHome1_1F/scripts.pas
+++
@@ -1,9 +0,0 @@
-PalletTown_NewHome1_1F_MapScripts::
-	.byte 0
-script PalletTown_NewHome1_1F_EventScript_Lass {
-    lock
-    faceplayer
-    msgbox(format("KANTO has grown quite a bit in recent years. You'll find new developments in many cities around the region just like here in Pallet Town."))
-    release
-    end
-}
